2650c95cfa51723e26a12281d461a4ea83e5facd
yuxiao <2286229152@qq.com>
2021-03-02 21:26:35 +0800

    rw_lock

---
+++ system_programming/Thread_sync/producer_consumer_cv.cc
@@ -0,0 +1,114 @@
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <functional>
+/*
+consumer0 ---get: Product 0
+consumer1 ---get: Product 1
+producer: The queue is FULL!!!
+consumer0 ---get: Product 2
+consumer1 ---get: Product 3
+producer: The queue is FULL!!!
+consumer0 ---get: Product 4
+......
+consumer0 quit!
+consumer1 quit!
+*/
+
+std::mutex mutex;
+std::condition_variable notFull;
+std::condition_variable notEmpty;
+std::queue<std::string> queue;
+const int maxSize = 5; // 最多存放maxSize个产品
+
+void consumer(int id)
+{
+    while(true)
+    {
+        std::string product;
+        {
+            std::unique_lock<std::mutex> lock(mutex); // 先取得锁再判断
+            while(queue.empty())  // 如果队列为空
+            {
+                // 解锁，阻塞等待条件满足
+                notEmpty.wait(lock);
+                // 当条件满足，该阻塞线程被唤醒；再次检查是否满足条件
+            }
+            // 条件满足，申请获得锁，取出产品
+            product = std::move(queue.front());
+            queue.pop();
+        }
+        // 释放锁
+        // 当前队列不满，通知生产者可以进行生产
+        notFull.notify_one();
+        if(product == "quit!")
+        {
+            printf("consumer%d quit!\n", id);
+            break;
+        }
+        printf("consumer%d ---get: %s\n", id, product.c_str());
+        sleep(1);
+    }
+}
+
+void producer()
+{
+    for(int i = 0;i < 20;++i)
+    {
+        {
+            std::unique_lock<std::mutex> lock(mutex);
+            while(queue.size() >= maxSize)  // 如果队列满了
+            {
+                printf("producer: The queue is FULL!!!\n");
+                notFull.wait(lock);
+            }
+            // 生产
+            std::string product("Product " + std::to_string(i));
+            queue.push(std::move(product));
+        }
+        // 通知消费者可以进行消费
+        notEmpty.notify_one();
+        usleep(5000);
+    }
+
+    for(int i = 0;i < 2;++i)
+    {
+        {
+            std::unique_lock<std::mutex> lock(mutex);
+            while(queue.size() >= maxSize)  // 如果队列满了
+            {
+                notFull.wait(lock);
+            }
+            // 提醒退出
+            queue.push("quit!");
+        }
+        // 通知消费者可以进行消费
+        notEmpty.notify_one();
+    }
+
+
+}
+
+int main()
+{
+    std::vector<std::thread> thrds;
+    thrds.reserve(3);
+
+    thrds.emplace_back(std::thread(producer));
+
+    for(int i = 0;i < 2;++i)
+    {
+        thrds.emplace_back(std::thread(std::bind(consumer, i)));
+    } 
+
+    for(auto& thrd : thrds)
+    {
+        thrd.join();
+    }
+    return 0;
+}
---
+++ system_programming/Thread_sync/rwlock_test.cc
@@ -0,0 +1,113 @@
+#include <unistd.h>
+#include <thread>
+#include <pthread.h> //rwlock
+#include <functional>
+#include <iostream>
+#include <vector>
+/*
+=====Writer 1 before: cnt = 669 after: cnt = 670   后面几个线程到来时
+=====Writer 0 before: cnt = 670 after: cnt = 671    写线程优先，独占
+-------------Reader 3, cnt = 671                    读线程在后，可共享
+-------------Reader 1, cnt = 671
+-------------Reader 0, cnt = 671
+-------------Reader 2, cnt = 671
+*/
+
+// RAII
+class writeLockGuard
+{
+public:
+    writeLockGuard(::pthread_rwlock_t* rwlock)
+        : rwlock_(rwlock)
+    {
+        pthread_rwlock_wrlock(rwlock_);
+    }
+
+    ~writeLockGuard()
+    {
+        pthread_rwlock_unlock(rwlock_);
+    }
+private:
+    pthread_rwlock_t* rwlock_;
+};
+
+class readLockGuard
+{
+public:
+    readLockGuard(::pthread_rwlock_t* rwlock)
+        : rwlock_(rwlock)
+    {
+        pthread_rwlock_rdlock(rwlock_);
+    }
+
+    ~readLockGuard()
+    {
+        pthread_rwlock_unlock(rwlock_);
+    }
+private:
+    pthread_rwlock_t* rwlock_;
+};
+
+pthread_rwlock_t rwlock;
+int global_counter = 0; // 共享资源
+
+void writerThread(int id)
+{
+    int save = 0;
+    while(true)
+    {
+       {
+            writeLockGuard lock_guard(&rwlock);
+            save = global_counter;
+            usleep(1000); // 该时间段内，依旧独占写锁
+            std::cout << "=====Writer " << id
+                    << " before: cnt = " <<  save
+                    << " after: cnt = " << ++global_counter << std::endl;
+       }
+    }
+    usleep(5000000);
+    
+}
+
+void readerThread(int id)
+{
+    while(true)
+    {
+        {
+            readLockGuard lock_guard(&rwlock);
+            printf("-------------Reader %d, cnt = %d\n", id, global_counter);
+        }
+        usleep(1000);
+    }
+}
+
+int main()
+{
+    std::vector<std::thread> thrds;
+    thrds.reserve(8);
+
+    pthread_rwlock_init(&rwlock, NULL);
+    // 3个写线程
+    for(int i = 0;i < 2;++i)
+    {
+        std::thread thrd(std::bind(writerThread, i));
+        thrds.emplace_back(std::move(thrd));
+    }
+
+    // 5个读线程
+    for(int i = 0;i < 4;++i)
+    {
+        std::thread thrd(std::bind(readerThread, i));
+        thrds.emplace_back(std::move(thrd));
+    }
+
+    for(auto& thrd : thrds)
+    {
+        thrd.join();
+    }
+
+    pthread_rwlock_destroy(&rwlock);
+    return 0;
+}
+
+
