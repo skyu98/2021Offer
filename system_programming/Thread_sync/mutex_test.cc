d8e86a1a1e0bfe8abda22c6bd9d9e16a474103cc
yuxiao <2286229152@qq.com>
2021-03-02 17:17:05 +0800

    thread_sync

---
+++ system_programming/Thread_sync/mutex_test.cc
@@ -0,0 +1,36 @@
+#include <thread>
+#include <mutex>
+#include <iostream>
+#include <unistd.h>
+
+// 此例中屏幕为共享资源，我们需要两个线程有序输出
+std::mutex mutex;
+
+void threadFunc()
+{
+    for(int i = 0;i < 50;++i)
+    {
+        {
+            std::lock_guard<std::mutex> lock(mutex);
+            std::cout << "hello ";
+            std::cout << "world" << std::endl;
+        }
+        sleep(1); // 确保其他线程获得锁
+    }
+};
+
+int main()
+{
+    std::thread lowerThread(threadFunc);
+    lowerThread.detach();
+    for(int i = 0;i < 50;++i)
+    {
+        {
+            std::lock_guard<std::mutex> lock(mutex);
+            std::cout << "HELLO ";
+            std::cout << "WORLD" << std::endl;
+        }
+        sleep(1);
+    }
+    return 0;
+}
